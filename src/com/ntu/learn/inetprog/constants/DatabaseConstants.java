7e38888449f20f883214c9995eb433fbe9ec5c28
Jim Aldon DSouza <jimaldon@gmail.com>
2015-10-22 18:37:50 +0800

    update> changed password to toor

--- src/com/ntu/learn/inetprog/constants/DatabaseConstants.java
+++ src/com/ntu/learn/inetprog/constants/DatabaseConstants.java
@@ -13,5 +13,5 @@ public class DatabaseConstants {
 	public static final String DB_DRIVER = "com.mysql.jdbc.Driver";
 	public static final String DB_CONNECTION = "jdbc:mysql://localhost:3306/ci6206";
 	public static final String DB_USER_NAME = "root";
-	public static final String DB_PASSWORD = "admin";
+	public static final String DB_PASSWORD = "toor";
 }
